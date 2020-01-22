4e6fb13ace22fe91a425f2f1fbeed5a733222421
lhermann <lhermann@student.42.fr>
2020-01-22 16:34:51 +0100

    [basics] - end the basics

--- app.js
+++ app.js
@@ -1,45 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require('./routes');
 
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>message</title></head>');
-    res.write('<body><form action="/message" method="POST">');
-    res.write('<input type="text" name="message"><button type="submit">Click me</button>');
-    res.write('</form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-    return req.on('end', () => { // if i dont put a return before req.on line 33 will be exectued first and do a ERR_HTTP_HEADERS_SENT error.
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, (err) => {
-        if (err != null) {
-          console.log(err);
-        } else {
-          console.log('message.txt successfully created and stored inside the server.');
-          res.statusCode = 302;
-          res.setHeader('Location', '/');
-          return res.end();
-        }
-      });
-    });
-  }
-  res.setHeader('Content-type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>Node Server</title></head>');
-  res.write('<body><h1>Welcome on my node.js server</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
