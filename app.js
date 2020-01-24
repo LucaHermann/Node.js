8a1a443805aa10d4896b49309d25a0ab628aa6a9
Luca HERMANN <lhermann@e1r8p21.42.fr>
2020-01-24 14:04:42 +0100

    [express] - better project management learn Routing

--- app.js
+++ app.js
@@ -3,19 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = ('./routes/admin');
+const shopRoutes = ('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res) => {
-  res.send('<h1>Hello from express</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
