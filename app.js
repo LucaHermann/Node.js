25eb7865b2fb093dbc3a06fb8add38bf15c3c63f
Luca HERMANN <lhermann@e1r8p21.42.fr>
2020-01-24 13:50:12 +0100

    [express] - better project stucture.

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('Always running');
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<h1>Inside add-product</h1>');
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
-app.use('/', (req, res, next) => {
+app.use('/', (req, res) => {
   res.send('<h1>Hello from express</h1>');
 });
 
