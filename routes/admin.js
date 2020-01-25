3e26bdf62541dd7d6d025b2c52bbc6171cd74b23
Luca HERMANN <lhermann@e1r7p1.42.fr>
2020-01-25 10:35:15 +0100

    [templating engine] - starting the module

--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,11 @@ const rootDir = require('../utils/path');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
+router.get('/add-product', (req, res) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/add-product', (req, res, next) => {
+router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
